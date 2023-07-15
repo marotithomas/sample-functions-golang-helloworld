be46a8d359b781a67a0c9d444e60a1ec891541b9
marotithomas <69324238+marotithomas@users.noreply.github.com>
2023-07-15 06:15:49 +0200

    Update hello.go

--- packages/sample/hello/hello.go
+++ packages/sample/hello/hello.go
@@ -1,25 +1,71 @@
 package main
 
 import (
-	"fmt"
+    "context"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+
+    "github.com/minio/minio-go/v7"
+    "github.com/minio/minio-go/v7/pkg/credentials"
 )
 
-type Request struct {
-	Name string `json:"name"`
+var (
+    minioClient *minio.Client
+    bucketName  string
+)
+
+func main() {
+    endpoint := "fra1.digitaloceanspaces.com"
+    accessKeyID := "DO00CXZFVFZBJP9FP4C6"
+    secretAccessKey := "ixuG36jRgBZjIouJ1w18Uig+nRKYXcdAshqeJqptfPI"
+    useSSL := true
+    bucketName = "teszt"
+
+    // Initialize minio client object.
+    var err error
+    minioClient, err = minio.New(endpoint, &minio.Options{
+        Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+        Secure: useSSL,
+    })
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    http.HandleFunc("/", handleRequest)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-type Response struct {
-	StatusCode int               `json:"statusCode,omitempty"`
-	Headers    map[string]string `json:"headers,omitempty"`
-	Body       string            `json:"body,omitempty"`
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    objectKey := r.URL.Query().Get("key")
+
+    if objectKey == "" {
+        http.Error(w, "Missing 'key' parameter", http.StatusBadRequest)
+        return
+    }
+
+    exists, err := checkObjectExists(objectKey)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    if exists {
+        fmt.Fprint(w, "OK")
+    } else {
+        fmt.Fprint(w, "FAILED")
+    }
 }
 
-func Main(in Request) (*Response, error) {
-	if in.Name == "" {
-		in.Name = "stranger"
-	}
-
-	return &Response{
-		Body: fmt.Sprintf("Hello %s!", in.Name),
-	}, nil
+func checkObjectExists(objectKey string) (bool, error) {
+    ctx := context.Background()
+    _, err := minioClient.StatObject(ctx, bucketName, objectKey, minio.StatObjectOptions{})
+    if err != nil {
+        if minio.ToErrorResponse(err).Code == "NoSuchKey" {
+            return false, nil
+        }
+        return false, err
+    }
+    return true, nil
 }
